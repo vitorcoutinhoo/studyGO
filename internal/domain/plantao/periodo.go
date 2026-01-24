4ab4d52caa43b0987cc01cec79cd4845604c353a
Vitor Coutinho <vitorcooutinho@gmail.com>
2026-01-24 10:33:27 -0300

    feat: creating Periodo function for dias

--- internal/domain/plantao/periodo.go
+++ internal/domain/plantao/periodo.go
@@ -5,6 +5,18 @@ import (
 	"time"
 )
 
+type DiaDaSemana int
+
+const (
+	Domingo DiaDaSemana = iota
+	SegundaFeira
+	TercaFeira
+	QuartaFeira
+	QuintaFeira
+	SextaFeira
+	Sabado
+)
+
 var (
 	ErrorEndBeforeStart  = errors.New("Data de término anterior à data de início!")
 	ErrorPeriodoInvalido = errors.New("Período Inválido!")
@@ -13,6 +25,12 @@ var (
 type Periodo struct {
 	Inicio time.Time
 	Fim    time.Time
+}
+
+type Dia struct {
+	Data      time.Time
+	DiaSemana DiaDaSemana
+	ehFeriado bool
 }
 
 func NewPeriodo(inicio, fim time.Time) (*Periodo, error) {
@@ -28,4 +46,22 @@ func NewPeriodo(inicio, fim time.Time) (*Periodo, error) {
 		Inicio: inicio,
 		Fim:    fim,
 	}, nil
+}
+
+func (p *Periodo) Dias(feriado map[time.Time]bool) []Dia {
+	var dias []Dia
+
+	data := p.Inicio
+	for !data.After(p.Fim) {
+		dia := Dia{
+			Data:      data,
+			DiaSemana: DiaDaSemana(data.Weekday()),
+			ehFeriado: feriado[data],
+		}
+
+		dias = append(dias, dia)
+		data = data.AddDate(0, 0, 1)
+	}
+
+	return dias
 }
