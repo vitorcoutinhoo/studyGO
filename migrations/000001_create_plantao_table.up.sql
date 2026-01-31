e1aeb0a0ab52e39bf9234cf28a10e83cf1a9c020
Vitor Coutinho <vitorcooutinho@gmail.com>
2026-01-31 09:00:21 -0300

    feat: creating migrations for plantao table

---
+++ migrations/000001_create_plantao_table.up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE plantoes (
+    id UUID NOT NULL DEFAULT gen_random_uuid(),
+    id_colaborador UUID NOT NULL,
+    data_inicio TIMESTAMP NOT NULL,
+    data_fim TIMESTAMP NOT NULL,
+    CONSTRAINT plantoes_pkey PRIMARY KEY(id),
+);
