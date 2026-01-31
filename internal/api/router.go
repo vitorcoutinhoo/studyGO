b2c0b2f35ff97a1676ff076a33635a32002cb5b6
Vitor Coutinho <vitorcooutinho@gmail.com>
2026-01-31 08:33:46 -0300

    feat: creating plantao endpoints

--- internal/api/router.go
+++ internal/api/router.go
@@ -1,7 +1,37 @@
 package api
 
-import "plantao/internal/api/controller"
+import (
+	"plantao/internal/api/controller"
+
+	"github.com/gin-gonic/gin"
+)
 
 func NewRouter(
 	plantaoController *controller.PlantaoController,
-)
+) *gin.Engine {
+	router := gin.Default()
+	setupPlantaoRoutes(router, plantaoController)
+	return router
+}
+
+func setupPlantaoRoutes(
+	router *gin.Engine,
+	plantaoController *controller.PlantaoController,
+) {
+	v1 := router.Group("/api/v1")
+	{
+		plantaoRoutes := v1.Group("/plantoes")
+		{
+			plantaoRoutes.POST("", plantaoController.CreatePlantao)
+			plantaoRoutes.GET("", plantaoController.GetPlantoes)
+			plantaoRoutes.GET("/:id", plantaoController.GetPlantaoById)
+			plantaoRoutes.DELETE("/:id", plantaoController.DeletePlantao)
+
+			plantaoRoutes.GET("/colaborador/:colaborador_id", plantaoController.GetPlantoesByColaboradorId)
+			plantaoRoutes.GET("/status/:status", plantaoController.GetPlantoesByStatus)
+			plantaoRoutes.GET("/:start_date/:end_date", plantaoController.GetPlantoesByPeriodo)
+
+			plantaoRoutes.PATCH("/:id/status", plantaoController.UpdateStatusPlantao)
+		}
+	}
+}
