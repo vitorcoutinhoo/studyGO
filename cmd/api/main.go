19e281861539ef2ec6cf3f5e9273ea884af33296
brunoatsoc <brunosantos20003237@gmail.com>
2026-04-21 15:12:51 -0300

    feat: adicionando declaração de logs, rate limite ao fx, e adicionando mensagens de log na inicialização e finalização do sistema

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	apihttp "plantao/internal/api/http"
 	"plantao/internal/api/middleware"
+	"plantao/internal/domain/log"
 	appfx "plantao/internal/fx"
 
 	"go.uber.org/fx"
@@ -17,16 +18,21 @@ func main() {
 		appfx.DomainModule,
 		appfx.APIModule,
 		appfx.FileModlule,
+		appfx.LogglerModule,
+		appfx.RateLimitModule,
 
 		fx.Invoke(middleware.StartRateLimitCleanup),
 
-		fx.Invoke(func(lc fx.Lifecycle, server *apihttp.Server) {
+		fx.Invoke(func(lc fx.Lifecycle, server *apihttp.Server, log log.Logger) {
 			lc.Append(fx.Hook{
 				OnStart: func(ctx context.Context) error {
 					go server.Start()
+					log.Info("Servidor iniciando...")
 					return nil
 				},
 				OnStop: func(ctx context.Context) error {
+					log.Info("Enserrando aplicação...")
+					defer log.Sync()
 					return server.Shutdown(ctx)
 				},
 			})
