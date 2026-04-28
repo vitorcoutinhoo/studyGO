ea0a62a8d6eb8e64567b63b22a6140f8edae8dc1
brunoatsoc <brunosantos20003237@gmail.com>
2026-04-28 10:32:20 -0300

    feat: adicionando envio de email na criação de um convite para novos usuários

--- internal/domain/convite/service.go
+++ internal/domain/convite/service.go
@@ -3,26 +3,58 @@ package convite
 import (
 	"context"
 	"fmt"
+	"log"
+	"plantao/internal/domain/comunicacao"
+	"plantao/internal/infra/config"
 
 	"github.com/google/uuid"
 )
 
 type ConviteService struct {
-	respository ConviteRepository
+	respository  ConviteRepository
+	envioService *comunicacao.EnvioService
+	urlServer    string
 }
 
-func NewConviteService(respository ConviteRepository) *ConviteService {
-	return &ConviteService{respository: respository}
+func NewConviteService(respository ConviteRepository, envioService *comunicacao.EnvioService, cfg *config.Config) *ConviteService {
+	return &ConviteService{
+		respository:  respository,
+		envioService: envioService,
+		urlServer:    "http://" + cfg.Server.Host + ":" + cfg.Server.Port + "/api/v1/usuarios/cadastro?token=",
+	}
 }
 
-func (s *ConviteService) CreateConvite(ctx context.Context, colaboradorId string) error {
+func (s *ConviteService) CreateConvite(ctx context.Context, colaboradorId, colaboradorNome, colaboradorEmail string) error {
 	id, err := uuid.Parse(colaboradorId)
 	if err != nil {
 		return fmt.Errorf("UUID inválido: %v", err)
 	}
 
-	_, err = s.respository.Store(ctx, id)
-	return err
+	conv, err := s.respository.Store(ctx, id)
+	if err != nil {
+		return fmt.Errorf("erro ao salvar convite: %v", err)
+	}
+
+	go func() {
+		data := map[string]any{
+			string(comunicacao.Nome):  colaboradorNome,
+			string(comunicacao.Email): colaboradorEmail,
+			string(comunicacao.Link):  s.urlServer + conv.Token.String(),
+		}
+
+		err := s.envioService.SendEmailComunicacao(
+			context.Background(),
+			comunicacao.ColaboradorCadastrado,
+			colaboradorEmail,
+			data,
+		)
+
+		if err != nil {
+			log.Printf("[email] erro ao enviar boas-vindas para %s: %v", colaboradorEmail, err)
+		}
+	}()
+
+	return nil
 }
 
 func (s *ConviteService) GetAllConvites(ctx context.Context) ([]Convite, error) {
