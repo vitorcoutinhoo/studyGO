b08311a3266061b23bc2ed7efc313179310eb07f
brunoatsoc <brunosantos20003237@gmail.com>
2026-09-14 20:57:34 -0300

    feat: alterando tempo para verificação de um plantão a ser iniciado pelo worker.

--- internal/worker/plantao_status_worker.go
+++ internal/worker/plantao_status_worker.go
@@ -11,7 +11,7 @@ import (
 	"go.uber.org/fx"
 )
 
-const plantaoStatusInterval = 5 * time.Minute
+const plantaoStatusInterval = 1 * time.Minute
 
 type plantaoStarter interface {
 	IniciarPlantoesAgendados(ctx context.Context) (int, error)
